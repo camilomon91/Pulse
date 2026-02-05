b05b629cbfada7ee8b0bbac1e69679691e9d194e
Camilo <cami92008@hotmail.com>
2026-02-05 15:17:08 -0500

    Initial Commit

---
+++ Pulse/PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseApp.swift
+//  Pulse
+//
+//  Created by Camilo Montero on 2026-02-05.
+//
+
+import SwiftUI
+
+@main
+struct PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
